aee83576a30132c3ab6571d7547aea296c0e9679
kouamemoayemorelyohan <m.kouame@alustudent.com>
2025-05-26 04:04:09 -0700

    second commit

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -4,11 +4,8 @@
 # Get the first argument passed to the script
 input = ARGV[0]
 
-# If no argument is provided, print empty string and exit
-if input.nil?
-  puts ""
-  exit
-end
+# Return empty string if no argument provided
+puts "" if input.nil?
 
 # Use scan to find all occurrences of "hbt{1,4}n" and join them
 matches = input.scan(/hbt{1,4}n/).join
